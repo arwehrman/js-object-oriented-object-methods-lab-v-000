f2833745a49a89ffdc61ed70db68fbf35e02faeb
Amanda Wehrman <arwehrman@gmail.com>
2018-01-21 17:39:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function BoardMember(name, homestate, training) {
-  this.name = name;
-  this.homestate = homestate;
-  this.training = training;
+  this.name = name
+  this.homestate = homestate
+  this.training = training
   this.veto = function(){
     console.log("No, I must disagree");
   this.approve = function(){
